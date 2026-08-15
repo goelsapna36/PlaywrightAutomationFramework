e9190486f5c8333b07e10b7366d752e7d16c7cf1
goelsapna36 <goelsapna36@gmail.com>
2026-08-15 15:33:47 +0530

    Test PR quality gate failure

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -11,6 +11,7 @@ test.describe('Login Tests', () => {
       testData.login.username,
       testData.login.password
     );
+    expect(1).toBe(2);
 
     await expect(page.locator('.title')).toHaveText('Products');
 
