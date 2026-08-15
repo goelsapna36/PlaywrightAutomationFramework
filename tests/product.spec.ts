4376ef9290421c967de1329ac06eeb9d7b49f1e4
goelsapna36 <goelsapna36@gmail.com>
2026-08-15 21:58:54 +0530

    latest

--- tests/product.spec.ts
+++ tests/product.spec.ts
@@ -1,21 +1,27 @@
-import{test,expect}from'../fixtures/testFixtures';
-import{testData}from'../utils/testData';
-import{ProductPage}from'../pages/ProductPage';
-
+import { test, expect } from '../fixtures/testFixtures';
+import { testData } from '../utils/testData';
+import { ProductPage } from '../pages/ProductPage';
 
 test(
   'User should login successfully and navigate to about page',
   { tag: '@regression' },
   async ({ page, loginPage }) => {
-  await page.goto('/');
 
-  await loginPage.login(
-    testData.login.username,
-    testData.login.password
-  );
+    await page.goto('/');
 
-  const productPage = new ProductPage(page);
-  await productPage.clickSettingIcon();
-  await productPage.clickaboutLink();
-  expect(page.url()).toContain('https://www.saucedemo.com/inventory.html');
-});
+    await loginPage.login(
+      testData.login.username,
+      testData.login.password
+    );
+
+    // Check URL after login
+    await expect(page).toHaveURL(
+      'https://www.saucedemo.com/inventory.html'
+    );
+
+    const productPage = new ProductPage(page);
+
+    await productPage.clickSettingIcon();
+    await productPage.clickaboutLink();
+  }
+);
