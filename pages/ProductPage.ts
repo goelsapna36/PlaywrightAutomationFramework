2024989d2abb7d0d5889c603df29a615094d1f33
goelsapna36 <goelsapna36@gmail.com>
2026-08-16 20:36:30 +0530

    Update project files

--- pages/ProductPage.ts
+++ pages/ProductPage.ts
@@ -5,21 +5,51 @@ export class ProductPage {
     readonly settingIcon: Locator;
     readonly logoutLink: Locator;
     readonly aboutLink: Locator;
+    readonly productName: Locator;
+    readonly productDescription: Locator;
+    readonly productPrice: Locator;
+    readonly addToCartButton: Locator;
+
+    readonly filterDropdown: Locator;
+    readonly filternameAtoZ: Locator;
+    readonly filternameZtoA: Locator
+
+    readonly filterPriceLowToHigh: Locator;
+    readonly filterPriceHighToLow: Locator;
+
+
 
     constructor(page: Page) {
         this.page = page;
 
-        this.settingIcon = page.locator('#react-burger-menu-btn');
-        this.aboutLink = page.locator('#about_sidebar_link');
-
-        // this.logoutLink = page.locator('[data-test="logout-sidebar-link"]');
+        // this.settingIcon = page.locator('#react-burger-menu-btn');
+        // this.aboutLink = page.locator('#about_sidebar_link');
+this.productName = page.locator('.inventory_item_name')
+this.productDescription = page.locator('.inventory_item_desc')
+this.productPrice = page.locator('[data-test="inventory-item-price"]')
+this.addToCartButton = page
+    .locator('.btn.btn_primary.btn_small.btn_inventory');
     }
 
-    async clickSettingIcon() {
-        await this.settingIcon.click();
-    }
+    // async clickSettingIcon() {
+    //     await this.settingIcon.click();
+    // }
 
-    async clickaboutLink() {
-        await this.aboutLink.click();
-    }
+    // async clickaboutLink() {
+    //     await this.aboutLink.click();
+ async getAllProductNames(): Promise<string[]> {
+    return await this.productName.allTextContents();
+}
+
+async getAllProductDescriptions(): Promise<string[]> {
+    return await this.productDescription.allTextContents();
+}
+
+async getAllProductPrices(): Promise<string[]> {
+    return await this.productPrice.allTextContents();
+}
+
+async getAllAddToCartButtons(): Promise<string[]> {
+    return await this.addToCartButton.allTextContents();
+}
 }
--- tests/product.spec.ts
+++ tests/product.spec.ts
@@ -3,25 +3,52 @@ import { testData } from '../utils/testData';
 import { ProductPage } from '../pages/ProductPage';
 
 test(
-  'User should login successfully and navigate to about page',
-  { tag: '@regression' },
-  async ({ page, loginPage }) => {
+    'User should login successfully and validate all products',
+    { tag: '@regression' },
+    async ({ page, loginPage }) => {
 
-    await page.goto('/');
+        await page.goto('/');
 
-    await loginPage.login(
-      testData.login.username,
-      testData.login.password
-    );
+        await loginPage.login(
+            testData.login.username,
+            testData.login.password
+        );
 
-    // Check URL after login
-    await expect(page).toHaveURL(
-     'https://www.saucedemo.com/inventory.html'
-    );
+        // Check URL after login
+        await expect(page).toHaveURL(
+            'https://www.saucedemo.com/inventory.html'
+        );
 
-    const productPage = new ProductPage(page);
+        const productPage = new ProductPage(page);
 
-    await productPage.clickSettingIcon();
-    await productPage.clickaboutLink();
-  }
+        // Get all product names
+        const productNames = await productPage.getAllProductNames();
+        console.log('Product Names:', productNames);
+
+        expect(productNames).toContain('Sauce Labs Backpack');
+
+        // Get all product descriptions
+        const productDescriptions =
+            await productPage.getAllProductDescriptions();
+
+        console.log('Product Descriptions:', productDescriptions);
+
+        expect(productDescriptions[0])
+            .toContain('carry.allTheThings');
+
+        // Get all product prices
+        const productPrices = await productPage.getAllProductPrices();
+
+        console.log('Product Prices:', productPrices);
+
+        expect(productPrices).toContain('$29.99');
+
+        // Get all Add to Cart button texts
+        const addToCartButtons =
+            await productPage.getAllAddToCartButtons();
+
+        console.log('Add to Cart Buttons:', addToCartButtons);
+
+        expect(addToCartButtons).toHaveLength(6);
+    }
 );
