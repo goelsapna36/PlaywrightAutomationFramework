19f5f967e6367214f6f8ec812d8ea1a543ec3bee
goelsapna36 <goelsapna36@gmail.com>
2026-08-16 09:11:29 +0530

    fixex

--- pages/ProductPage.ts
+++ pages/ProductPage.ts
@@ -8,19 +8,18 @@ export class ProductPage {
 
     constructor(page: Page) {
         this.page = page;
+
         this.settingIcon = page.locator('#react-burger-menu-btn');
         this.aboutLink = page.locator('#about_sidebar_link');
 
-        //this.logoutLink =page.locator('[data-test="logout-sidebar-link"]');
+        // this.logoutLink = page.locator('[data-test="logout-sidebar-link"]');
     }
 
     async clickSettingIcon() {
         await this.settingIcon.click();
-    }       
+    }
+
     async clickaboutLink() {
         await this.aboutLink.click();
     }
-    // async clicklogoutLink() {
-    //     await this.logoutLink.click();
-
 }
